4f29bb728d207caba0d4a366b3207bc1b03b990b
Punit Shukla <punit27nov@gmail.com>
2025-08-03 03:12:37 +0530

    added half hero section with horizontal scrolling of cards

--- src/Components/TitleCards/TitleCards.jsx
+++ src/Components/TitleCards/TitleCards.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
+import cards_data from "../../assets/assets/cards/Cards_data.js";
+
 const TitleCards = () => {
-  return <div>TitleCards</div>;
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+    return () => {
+      currentRef.removeEventListener("wheel", handleWheel);
+    };
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../Components/Navbar/Navbar.jsx";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/assets/hero_title.png";
+import play_icon from "../../assets/assets/play_icon.png";
+import info_icon from "../../assets/assets/info_icon.png";
+import TitleCards from "../../Components/TitleCards/TitleCards.jsx";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ad
+            placeat illo cum dignissimos recusandae nulla? Maiores illo
+            recusandae optio, ad officiis velit laborum nostrum.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More info
+            </button>
+          </div>
+          <TitleCards />
+        </div>
+      </div>
     </div>
   );
 };
